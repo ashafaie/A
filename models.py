170a6f17b9c900e3e14e0f72649a17693443987b
Arash Shafaie <ashafaieuni@gmail.com>
2014-10-09 11:25:50 +0330

    On branch admin  Changes to be committed: 	modified:   models.py

--- models.py
+++ models.py
@@ -1,12 +1,21 @@
 
 from sqlalchemyinit import db
 
+class Role (db.Model):
+    
+    __tablename__ = 'roles'
+    id = db.Column (db.Integer, primary_key=True)
+    name = db.Column (db.String(64), unique=True)
+    users = db.relationship ('User', backref='role')
+
 class User (db.Model):
 
-	id = db.Column (db.Integer, primary_key=True)
-	username = db.Column (db.String, unique=True, index=True)
-	password = db.Column (db.String)
+    __tablename__ = 'users'
+    id = db.Column (db.Integer, primary_key=True)
+    username = db.Column (db.String, unique=True, index=True)
+    password = db.Column (db.String)
+    role_id = db.Column (db.Integer, db.ForeignKey('roles.id'))
 
-	def __repr__ (self):
-		return self.username
+    def __repr__ (self):
+            return self.username
 
